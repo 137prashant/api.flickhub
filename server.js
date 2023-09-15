427e49ebedb052b26931edfbc8f872d84a70a59a
137prashant <prashantsinghalpq@gmail.com>
2023-09-15 15:49:01 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path')
 
 connectDb();
 
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname + "/public")))
 app.use("/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
