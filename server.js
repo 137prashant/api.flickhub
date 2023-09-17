98ea2861436945288b67baa0ff24784a0939a494
137prashant <prashantsinghalpq@gmail.com>
2023-09-17 21:51:31 +0530

    updated6

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const path = require('path')
 
 connectDb();
 
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
+
+
 app.use(express.json());
 app.use(cors());
 
