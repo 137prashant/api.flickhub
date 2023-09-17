893fb80b1c732773afc05d8b87ecf4e57df5123c
137prashant <prashantsinghalpq@gmail.com>
2023-09-17 17:36:02 +0530

    updated4

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ connectDb();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use("/users", require("./routes/userRoutes"));
 
